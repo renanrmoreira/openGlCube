b6ecdd1748073580d569fa5cb7c680ab690bf0b2
Renan Rodrigues Moreira <98842653+renanrmoreira@users.noreply.github.com>
2025-03-30 23:24:03 -0300

    Add files via upload

--- PyCube/pythonProject/Cubo.py
+++ PyCube/pythonProject/Cubo.py
@@ -2,13 +2,22 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import numpy as np
+import math
 
 
 def transform_vertex(vertex, translation=(0, 0, 0), scale=(1, 1, 1), mirror=(1, 1, 1)):
+    """
+    Aplica transformações geométricas em um vértice:
+    - Translação: move o vértice no espaço
+    - Escala: aumenta ou diminui o tamanho
+    - Espelhamento: inverte coordenadas (usando valor -1)
+    """
     x, y, z = vertex
+
     x *= scale[0] * mirror[0]
     y *= scale[1] * mirror[1]
     z *= scale[2] * mirror[2]
+
     x += translation[0]
     y += translation[1]
     z += translation[2]
@@ -29,14 +38,43 @@ edges = [(0, 1), (1, 3), (3, 2), (2, 0),
          (0, 6), (1, 7), (2, 4), (3, 5)]
 
 
-def wireCube():
-    translation = (0.5, 0.5, 0)  # Translação (x, y, z)
-    scale = (1.5, 1.5, 1.5)  # Escala (x, y, z)
-    mirror = (-1, 1, 1)  # Espelhamento (x, y, z) (-1 inverte eixo correspondente)
+def draw_cube(translation, scale, mirror):
 
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
+            # aplica as transformações em cada vertice
             transformed_vertex = transform_vertex(vertices[vertex], translation, scale, mirror)
             glVertex3fv(transformed_vertex)
     glEnd()
+
+
+def draw_cubes(rotation_angle, time):
+    # Calcula a escala usando uma função senoidal para criar um efeito pulsante
+    scale_factor = 1.4 + 0.6 * math.sin(time * 0.05)
+
+    # Calcula a translação horizontal usando uma função senoidal e faz os cubos moverem-se horizontalmente
+    x_offset = 1.0 * math.sin(time * 0.03)
+
+    # distância base entre os cubos
+    base_distance = 2.0
+
+    scale = (scale_factor, scale_factor, scale_factor)  # Escala animada para ambos os cubos
+
+    # Cubo da esquerda (normal, rotação no sentido positivo)
+    glPushMatrix()
+    # Posiciona à esquerda e adiciona o deslocamento animado
+    glTranslatef(-base_distance + x_offset, 0.0, 0.0)
+    # Rotação normal
+    glRotatef(rotation_angle, 1, 1, 1)
+    draw_cube(translation=(0, 0, 0), scale=scale, mirror=(1, 1, 1))
+    glPopMatrix()
+
+    # Cubo da direita (espelhado, rotação no sentido negativo)
+    glPushMatrix()
+    # Posiciona à direita e adiciona o mesmo deslocamento animado
+    glTranslatef(base_distance + x_offset, 0.0, 0.0)
+    # Rotação em sentido oposto (valores negativos)
+    glRotatef(-rotation_angle, 1, 1, 1)
+    draw_cube(translation=(0, 0, 0), scale=scale, mirror=(-1, 1, 1))  # Espelhamento no eixo X
+    glPopMatrix()
--- PyCube/pythonProject/Main.py
+++ PyCube/pythonProject/Main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import Cubo
+import math
 
 pygame.init()
 
@@ -12,7 +13,8 @@ background_color = (0, 0, 0, 1)
 drawing_color = (1, 1, 1, 1)
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
-pygame.display.set_caption('OpenGL in Python')
+pygame.display.set_caption('Cubos com Rotação Espelhada, Movimento e Escala')
+
 
 def initialise():
     glClearColor(background_color[0], background_color[1], background_color[2], background_color[3])
@@ -24,25 +26,45 @@ def initialise():
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    glTranslate(0, 0, -5)
+    glTranslate(0, 0, -8)
     glViewport(0, 0, screen.get_width(), screen.get_height())
     glEnable(GL_DEPTH_TEST)
 
-def display():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glRotatef(1, 1, 1, 1)
-    glPushMatrix()
-    Cubo.wireCube()
-    glPopMatrix()
 
-done = False
+# controla o ângulo de rotação
+rotation_angle = 0
+# contador de tempo para animar escala e translação
+time_counter = 0
+
+
+def display():
+    global rotation_angle, time_counter
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    rotation_angle += 1
+    time_counter += 1
+
+    Cubo.draw_cubes(rotation_angle, time_counter)
+
+    pygame.display.flip()
+
 initialise()
+
+# configura o relógio para controle de FPS
 clock = pygame.time.Clock()
+
+# Loop principal
+done = False
 while not done:
+    # Processa eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
     display()
-    pygame.display.flip()
+
+    # 60 FPS
     clock.tick(60)
+
 pygame.quit()
